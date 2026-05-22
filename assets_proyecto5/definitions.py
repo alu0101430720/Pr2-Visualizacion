4a9b5ca5577bf2ef2486be168b5a4d9c997b9bba
Carlos Yanes <alu0101430720@ull.edu.es>
2026-05-22 14:32:53 +0100

    refactorizado de código

--- assets_proyecto5/definitions.py
+++ assets_proyecto5/definitions.py
@@ -2,9 +2,8 @@ from dagster import Definitions, load_assets_from_modules, load_asset_checks_fro
 import assets
 import checks_p5
 import plots_assets
-import plots_assets_contratos_canarias
 
-all_assets = load_assets_from_modules([assets, plots_assets, plots_assets_contratos_canarias])
+all_assets = load_assets_from_modules([assets, plots_assets])
 all_checks = load_asset_checks_from_modules([checks_p5])
 
 defs = Definitions(
